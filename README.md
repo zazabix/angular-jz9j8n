6fb8505b7cb81362adae2b80fd42ba53b610aab6
zazabix <cloud_titan@yahoo.com>
2020-12-17 03:59:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jz9j8n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jz9j8n)
---
+++ src/app/cdk-drag-drop-free-drag-position-example.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Programmatically setting the free drag position
+ */
+@Component({
+  selector: 'cdk-drag-drop-free-drag-position-example',
+  templateUrl: 'cdk-drag-drop-free-drag-position-example.html',
+  styleUrls: ['cdk-drag-drop-free-drag-position-example.css'],
+})
+export class CdkDragDropFreeDragPositionExample {
+  dragPosition = {x: 0, y: 0};
+
+  changePosition() {
+    this.dragPosition = {x: this.dragPosition.x + 50, y: this.dragPosition.y + 50};
+  }
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
